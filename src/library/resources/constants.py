e064d9b8d126dfb071ff477dc3981ebcdd0cbdf2
twtr.p <piraboon77@gmail.com>
2024-11-07 01:50:06 +0700

    enabled music

--- src/library/resources/constants.py
+++ src/library/resources/constants.py
@@ -4,7 +4,7 @@ screen_width = 1280    #default: 1280
 screen_height = 720    #default: 720
 max_fps = 30
 
-music_volume = 0    #default: 1
-ambience_volume = 0    #default: 0.2
+music_volume = 1    #default: 1
+ambience_volume = 0.2    #default: 0.2
 
 skip_bootup = False
