b75ef29314d29d3e1ea8b8563cd5c4f5c84e5ef7
ThreeSIIT <threeariya@gmail.com>
2024-11-08 16:22:22 +0700

    Change box render code in Play_StartStatr
    Change draw_rect to use method in util instead

--- src/states/Play_StartState.py
+++ src/states/Play_StartState.py
@@ -9,39 +9,33 @@ class Play_StartState(BaseState):
         self.load_assets()
 
     def load_assets(self):
-        canvas_width = self.game.canvas.get_width()
-        canvas_height = self.game.canvas.get_height()
-
-        # Load white side box
-        self.boxes = []
-        self.box_color = (*colors.white, 191) # 75% transparency (191)
-        self.border_color = colors.black
-        self.border_thickness = 3
-        box_width = 265
-        
-        left_box_rect = pygame.Rect(self.border_thickness, self.border_thickness, box_width, canvas_height - 2 * self.border_thickness)
-        right_box_rect = pygame.Rect(canvas_width - box_width - self.border_thickness, self.border_thickness, box_width, canvas_height - 2 * self.border_thickness)
-
-        self.boxes = [left_box_rect, right_box_rect]
-
-        self.transparent_surface = pygame.Surface((canvas_width, canvas_height), pygame.SRCALPHA)
+        pass
 
     def update(self, dt, events):
         utils.set_cursor(cursor=self.cursor)
         self.cursor = cursors.normal
 
     def render(self, canvas):
-        self.transparent_surface.fill((0, 0, 0, 0))
+        canvas_width = self.game.canvas.get_width()
+        canvas_height = self.game.canvas.get_height()
 
-        # Render boxes in the list
-        for box in self.boxes:
-            border_rect = pygame.Rect(
-                box.x - self.border_thickness,
-                box.y - self.border_thickness,
-                box.width + 2 * self.border_thickness,
-                box.height + 2 * self.border_thickness
-            )
-            pygame.draw.rect(self.transparent_surface, self.border_color, border_rect)
-            pygame.draw.rect(self.transparent_surface, self.box_color, box)
+        # Render left white box
+        utils.draw_rect(dest=canvas,
+                            size=(272, canvas_height),
+                            pos=(0, 0),
+                            pos_anchor='topleft',
+                            color=(*colors.white, 191), # 75% transparency
+                            inner_border_width=4,
+                            outer_border_width=0,
+                            outer_border_color=colors.black)
         
-        canvas.blit(self.transparent_surface, (0, 0))
+        # Render right white box
+        utils.draw_rect(dest=canvas,
+                            size=(272, canvas_height),
+                            pos=(canvas_width - 272, 0),
+                            pos_anchor='topleft',
+                            color=(*colors.white, 191), # 75% transparency
+                            inner_border_width=4,
+                            outer_border_width=0,
+                            outer_border_color=colors.black)
+        
