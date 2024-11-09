288e5a3935312f6847c857e6073b82c2a8fc412b
6422781664-KrittidechPai <6422781664@g.siit.tu.ac.th>
2024-11-10 04:45:25 +0700

    relogic the organize_deck function to make it complies with our game rule that there can't be 3 consecutive strike cards at the start of the day

--- src/library/deck.py
+++ src/library/deck.py
@@ -72,28 +72,39 @@ class Deck:
             else:
                 self.cards.append(Card(self.deck_type, i, True))
     
-    # This function make sure that there will be no 3 strike cards in a row in a day
+    # This function make sure that there will be no 3 strike cards in a row at the start of a day
     def organize_deck(self):
         disorganized = True
         while disorganized:
-            strike_index = []
-            strike_count = 0
+            strike_index = []   
+            strike_consecutive_count = 0    # This count strikes that are consecutive
+            strike_count = 0    # This count strikes to keep track of days
+            start_of_today_index = 0    # This keep track of the strike index at the start of today
+            start_of_next_day_index = 0   # This keep track of the strike index at the start of the next day
             disorganized = False # The deck is clean until proven otherwise
             for i, card in enumerate(self.cards):
                 if card.strike:
                     strike_count += 1
+                    strike_consecutive_count += 1
                     strike_index.append(i)
+                    # The third strike has been reached, update the start of the day
                     if strike_count == 3:
-                        if strike_index[-1] - strike_index[0] == 2:
+                        start_of_today_index = start_of_next_day_index
+                        start_of_next_day_index = i + 1
+                        strike_count = 0
+                    # The third strike has been reached consecutively
+                    if strike_consecutive_count == 3:
+                        # if it is at the start of the day, reshuffle and restart
+                        if strike_index[0] == start_of_today_index:
                             random.shuffle(self.cards)
                             disorganized = True
                             break
                         else:
                             strike_index = []
-                            strike_count = 0
+                            strike_consecutive_count = 0
                 else:
                     strike_index = []
-                    strike_count = 0
+                    strike_consecutive_count = 0
 
     def verify_consecutive_strike(self):
         strike_indices = []
