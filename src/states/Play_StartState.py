5bbffa8b002c9223762e55928efc7561759dad8b
ThreeSIIT <threeariya@gmail.com>
2024-11-09 19:25:26 +0700

    Add right box text in Play_StartState
    Add title, deck type, remaining

--- src/states/Play_StartState.py
+++ src/states/Play_StartState.py
@@ -9,6 +9,7 @@ class Play_StartState(BaseState):
         self.load_assets()
 
     def load_assets(self):
+        # left text
         self.left_box_title = utils.get_text(text='Score', font=fonts.lf2, size='small', color=colors.white)
 
         self.score_list = [
@@ -33,6 +34,22 @@ class Play_StartState(BaseState):
 
         self.left_box_path_text = utils.get_text(text='Place drawn path', font=fonts.lf2, size='tiny', color=colors.white)
 
+        # right text
+        self.right_box_title = utils.get_text(text='Cards', font=fonts.lf2, size='small', color=colors.white)
+
+        self.deck_list = [
+            {'text': 'Fruit',},
+            {'text': 'Path',},
+            {'text': 'Event',},
+        ]
+
+        self.deck_title_list = []
+        for score in self.deck_list:
+            text = utils.get_text(text=score['text'], font=fonts.lf2, size='smaller', color=colors.white)
+            self.deck_title_list.append(text)
+
+        self.right_remaining = utils.get_text(text='Remaining', font=fonts.lf2, size='smaller', color=colors.white)
+        
 
     def update(self, dt, events):
         utils.set_cursor(cursor=self.cursor)
@@ -74,5 +91,11 @@ class Play_StartState(BaseState):
                             outer_border_width=0,
                             outer_border_color=colors.black)
         
+        # Render text in right white box
+        utils.blit(dest=canvas, source=self.right_box_title, pos=(constants.canvas_width - box_width/2, 40), pos_anchor='center')
+        for i, deck in enumerate(self.deck_title_list):
+            utils.blit(dest=canvas, source=deck, pos=(1150, 90 + i*125), pos_anchor='topleft')
+            utils.blit(dest=canvas, source=self.right_remaining, pos=(1150, 115 + i*125), pos_anchor='topleft')
 
-        
+        # Render strike box in right white box
+        scaled_blank_strike = pygame.transform.scale_by(surface=self.blank_strike_image, factor=0.625)
