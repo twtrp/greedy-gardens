8c806e18bba444439611b1a6480aef7873f3a2bd
6422781441 <6422781441@g.siit.tu.ac.th>
2024-11-18 20:31:43 +0700

    done with resultState

--- src/states/Play_ResultState.py
+++ src/states/Play_ResultState.py
@@ -9,34 +9,34 @@ class Play_ResultStage(BaseState):
         self.parent = parent
         self.load_assets()
         
-        # Get high score from database
-        
-        
-        # Save current score to database
-        #do later
-        """conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        cursor.execute('''
-            INSERT INTO `records` (`score`, `seed`, `seed_type`) VALUES
-                (50, 12345678, 'Set Seed'),
-                (40, 23456789, 'Random Seed'),
-                (30, 34567890, 'Random Seed'),
-                (20, 45678901, 'Set Seed'),
-                (10, 56789012, 'Random Seed'),
-                (50, 23456789, 'Set Seed'),
-                (60, 23456789, 'Set Seed') 
-        ''')
-        conn.commit()
-        conn.close()"""
 
     def load_assets(self):
         
+        self.new_record = False
+        
         sql_conn = sqlite3.connect('data/records.sqlite')
         sql_cursor = sql_conn.cursor()
-        sql_cursor.execute('SELECT MAX(score) FROM records')
-        self.high_score = sql_cursor.fetchall()[0][0]
+        if self.parent.set_seed:
+            # Check if seed exists
+            sql_cursor.execute('SELECT COUNT(*) FROM records WHERE seed = ?', (self.parent.seed,))
+            seed_exists = sql_cursor.fetchone()[0] > 0
+            if not seed_exists:
+                self.high_score = 0
+            else:
+                sql_cursor.execute('SELECT MAX(score) FROM records WHERE seed = ?', (self.parent.seed,))
+                self.high_score = sql_cursor.fetchone()[0]
+        else:
+            sql_cursor.execute('SELECT COUNT(*) FROM records')
+            records_exist = sql_cursor.fetchone()[0] > 0
+            if not records_exist:
+                self.high_score = 0
+            else:
+                sql_cursor.execute('SELECT MAX(score) FROM records')
+                self.high_score = sql_cursor.fetchone()[0]
+                
         sql_cursor.close()
-        #print(self.high_score)
+        sql_conn.close()
+
         
         self.is_hovering=False
         self.button_list = []
@@ -45,7 +45,7 @@ class Play_ResultStage(BaseState):
         if self.parent.set_seed:
             self.seed_text = utils.get_text(text=f'Set Seed - {self.parent.seed}', font=fonts.lf2, size='medium', color=colors.white)
         elif self.parent.set_seed:
-            self.seed_text = utils.get_text(text=f'Random Seed - {self.parent.seed}', font=fonts.lf2, size='medium', color=colors.white)
+            self.seed_text = utils.get_text(text=f'Random Seed', font=fonts.lf2, size='medium', color=colors.white)
             
         self.seed_text = utils.get_text(text=f'Set Seed - {self.parent.seed}', font=fonts.lf2, size='medium', color=colors.white)
         
@@ -64,24 +64,26 @@ class Play_ResultStage(BaseState):
         self.big_day4_fruit_image = pygame.transform.scale_by(surface=self.day4_fruit_color_image, factor=2)
         self.big_seasonal_fruit_image = pygame.transform.scale_by(surface=self.seasonal_fruit_color_image, factor=2)
         
-        self.day1_text = utils.get_text(text="Day 1", font=fonts.lf2, size='medium', color=colors.white)
-        self.day2_text = utils.get_text(text='Day 2', font=fonts.lf2, size='medium', color=colors.white)
-        self.day3_text = utils.get_text(text='Day 3', font=fonts.lf2, size='medium', color=colors.white)
-        self.day4_text = utils.get_text(text='Day 4', font=fonts.lf2, size='medium', color=colors.white)
-        self.seasonal_text = utils.get_text(text='Seasonal', font=fonts.lf2, size='medium', color=colors.white)
+        self.day1_text = utils.get_text(text="Day 1", font=fonts.lf2, size='tiny', color=colors.white)
+        self.day2_text = utils.get_text(text='Day 2', font=fonts.lf2, size='tiny', color=colors.white)
+        self.day3_text = utils.get_text(text='Day 3', font=fonts.lf2, size='tiny', color=colors.white)
+        self.day4_text = utils.get_text(text='Day 4', font=fonts.lf2, size='tiny', color=colors.white)
+        self.seasonal_text = utils.get_text(text='Seasonal', font=fonts.lf2, size='tiny', color=colors.white)
         
         # Convert the score to string when calling get_text
-        self.day1_score_text = utils.get_text(text=str(self.parent.final_day1_score),font=fonts.lf2,size='medium', color=colors.white)
-        self.day2_score_text = utils.get_text(text=str(self.parent.final_day2_score), font=fonts.lf2, size='medium', color=colors.white)
-        self.day3_score_text = utils.get_text(text=str(self.parent.final_day3_score), font=fonts.lf2, size='medium', color=colors.white)
-        self.day4_score_text = utils.get_text(text=str(self.parent.final_day4_score), font=fonts.lf2, size='medium', color=colors.white)
-        self.seasonal_score_text = utils.get_text(text=str(self.parent.final_seasonal_score), font=fonts.lf2, size='medium', color=colors.white)
+        self.day1_score_text = utils.get_text(text=str(self.parent.final_day1_score),font=fonts.lf2,size='small', color=colors.white)
+        self.day2_score_text = utils.get_text(text=str(self.parent.final_day2_score), font=fonts.lf2, size='small', color=colors.white)
+        self.day3_score_text = utils.get_text(text=str(self.parent.final_day3_score), font=fonts.lf2, size='small', color=colors.white)
+        self.day4_score_text = utils.get_text(text=str(self.parent.final_day4_score), font=fonts.lf2, size='small', color=colors.white)
+        self.seasonal_score_text = utils.get_text(text=str(self.parent.final_seasonal_score), font=fonts.lf2, size='small', color=colors.white)
         
-        self.total_text = utils.get_text(text='Total', font=fonts.lf2, size='medium', color=colors.white)
-        self.total_score_text = utils.get_text(text=str(self.parent.total_score), font=fonts.lf2, size='medium', color=colors.white)
+        self.total_text = utils.get_text(text='Total', font=fonts.lf2, size='tiny', color=colors.white)
+        self.total_score_text = utils.get_text(text=str(self.parent.total_score), font=fonts.lf2, size='small', color=colors.white)
         
-        self.high_score_text = utils.get_text(text='High Score', font=fonts.lf2, size='medium', color=colors.white)
-        self.high_score_fromDB_text = utils.get_text(text=str(self.high_score), font=fonts.lf2, size='medium', color=colors.white)
+        self.high_score_text = utils.get_text(text='High Score', font=fonts.lf2, size='tiny', color=colors.white)
+        self.high_score_fromDB_text = utils.get_text(text=str(self.high_score), font=fonts.lf2, size='small', color=colors.white)
+        
+        self.new_text=utils.get_text(text='New ', font=fonts.lf2, size='tiny', color=colors.green_medium)
         
         self.button_option_list = [
             {
@@ -122,8 +124,48 @@ class Play_ResultStage(BaseState):
                     hover_cursor=cursors.hand,
                 ))
         self.parent.endDayState=True
-        # First load the sprite sheet
         
+        sql_conn = sqlite3.connect('data/records.sqlite')
+        sql_cursor = sql_conn.cursor()
+        if self.parent.set_seed:
+            sql_cursor.execute('SELECT MAX(score) FROM records WHERE seed = ?', (self.parent.seed,))
+            result = sql_cursor.fetchone()
+            current_high_score = result[0] if result[0] is not None else 0
+            
+            if self.parent.total_score > current_high_score:
+                self.high_score = self.parent.total_score
+                self.new_record = True
+            else:
+                self.high_score = current_high_score
+                
+            # Insert the new score
+            sql_cursor.execute(
+                'INSERT INTO records (score, seed, seed_type) VALUES (?, ?, ?)',
+                (self.parent.total_score, self.parent.seed, 'Set Seed')
+            )
+
+        else:
+            # Check highest score for random seeds
+            sql_cursor.execute('SELECT MAX(score) FROM records WHERE seed_type = "Random Seed"')
+            result = sql_cursor.fetchone()
+            current_high_score = result[0] if result[0] is not None else 0
+            
+            if self.parent.total_score > current_high_score:
+                self.high_score = self.parent.total_score
+                self.new_record = True
+            else:
+                self.high_score = current_high_score
+                
+            # Insert the new score
+            sql_cursor.execute(
+                'INSERT INTO records (score, seed, seed_type) VALUES (?, ?, ?)',
+                (self.parent.total_score, self.parent.seed, 'Random Seed')
+            )
+
+        sql_conn.commit()
+        sql_cursor.close()
+        sql_conn.close()
+                
 
         
     def update(self, dt, events):
@@ -170,32 +212,33 @@ class Play_ResultStage(BaseState):
             
             utils.blit(dest=canvas, source=self.seed_text, pos=(constants.canvas_width/2, 100), pos_anchor='center')
   
-            utils.blit(dest=canvas, source=self.big_day1_fruit_image, pos=(constants.canvas_width/2 - 175, 150), pos_anchor=posanchors.topleft)
+            utils.blit(dest=canvas, source=self.big_day1_fruit_image, pos=(constants.canvas_width/2 - 175, 140), pos_anchor=posanchors.topleft)
             utils.blit(dest=canvas, source=self.day1_text, pos=((constants.canvas_width/2 )-135, 140), pos_anchor=posanchors.topleft)
-            utils.blit(dest=canvas, source=self.day1_score_text, pos=((constants.canvas_width/2 )+130, 140), pos_anchor=posanchors.topright)
+            utils.blit(dest=canvas, source=self.day1_score_text, pos=((constants.canvas_width/2 )+160, 140-10), pos_anchor=posanchors.topright)
             
-            utils.blit(dest=canvas, source=self.big_day2_fruit_image, pos=(constants.canvas_width/2 - 175, 210), pos_anchor=posanchors.topleft)
+            utils.blit(dest=canvas, source=self.big_day2_fruit_image, pos=(constants.canvas_width/2 - 175, 200), pos_anchor=posanchors.topleft)
             utils.blit(dest=canvas, source=self.day2_text, pos=((constants.canvas_width/2 )-135, 200), pos_anchor=posanchors.topleft)
-            utils.blit(dest=canvas, source=self.day2_score_text, pos=((constants.canvas_width/2 )+130, 200), pos_anchor=posanchors.topright)
+            utils.blit(dest=canvas, source=self.day2_score_text, pos=((constants.canvas_width/2 )+160, 200-10), pos_anchor=posanchors.topright)
             
-            utils.blit(dest=canvas, source=self.big_day3_fruit_image, pos=(constants.canvas_width/2 - 175, 270), pos_anchor=posanchors.topleft)
+            utils.blit(dest=canvas, source=self.big_day3_fruit_image, pos=(constants.canvas_width/2 - 175, 260), pos_anchor=posanchors.topleft)
             utils.blit(dest=canvas, source=self.day3_text, pos=((constants.canvas_width/2 )-135, 260), pos_anchor=posanchors.topleft)
-            utils.blit(dest=canvas, source=self.day3_score_text, pos=((constants.canvas_width/2 )+130, 260), pos_anchor=posanchors.topright)
+            utils.blit(dest=canvas, source=self.day3_score_text, pos=((constants.canvas_width/2 )+160, 260-10), pos_anchor=posanchors.topright)
             
-            utils.blit(dest=canvas, source=self.big_day4_fruit_image, pos=(constants.canvas_width/2 - 175, 330), pos_anchor=posanchors.topleft)
+            utils.blit(dest=canvas, source=self.big_day4_fruit_image, pos=(constants.canvas_width/2 - 175, 320), pos_anchor=posanchors.topleft)
             utils.blit(dest=canvas, source=self.day4_text, pos=((constants.canvas_width/2 )-135, 320), pos_anchor=posanchors.topleft)
-            utils.blit(dest=canvas, source=self.day4_score_text, pos=((constants.canvas_width/2 )+130, 320), pos_anchor=posanchors.topright)
+            utils.blit(dest=canvas, source=self.day4_score_text, pos=((constants.canvas_width/2 )+160, 320-10), pos_anchor=posanchors.topright)
             
-            utils.blit(dest=canvas, source=self.big_seasonal_fruit_image, pos=(constants.canvas_width/2 - 175, 390), pos_anchor=posanchors.topleft)
+            utils.blit(dest=canvas, source=self.big_seasonal_fruit_image, pos=(constants.canvas_width/2 - 175, 380), pos_anchor=posanchors.topleft)
             utils.blit(dest=canvas, source=self.seasonal_text, pos=((constants.canvas_width/2 )-135, 380), pos_anchor=posanchors.topleft)
-            utils.blit(dest=canvas, source=self.seasonal_score_text, pos=((constants.canvas_width/2 )+130, 380), pos_anchor=posanchors.topright)
+            utils.blit(dest=canvas, source=self.seasonal_score_text, pos=((constants.canvas_width/2 )+160, 380-10), pos_anchor=posanchors.topright)
             
             utils.blit(dest=canvas, source=self.total_text, pos=(constants.canvas_width/2 - 135, 440), pos_anchor=posanchors.topleft)
-            utils.blit(dest=canvas, source=self.total_score_text, pos=((constants.canvas_width/2 )+130, 440), pos_anchor=posanchors.topright)
+            utils.blit(dest=canvas, source=self.total_score_text, pos=((constants.canvas_width/2 )+160, 440-10), pos_anchor=posanchors.topright)
             
             utils.blit(dest=canvas, source=self.high_score_text ,pos=(constants.canvas_width/2 -135, 500), pos_anchor=posanchors.topleft)
-            utils.blit(dest=canvas, source=self.high_score_fromDB_text, pos=((constants.canvas_width/2 )+130, 500), pos_anchor=posanchors.topright)
-
+            utils.blit(dest=canvas, source=self.high_score_fromDB_text, pos=((constants.canvas_width/2 )+160, 500-10), pos_anchor=posanchors.topright)
+            if self.new_record:
+                utils.blit(dest=canvas, source=self.new_text, pos=((constants.canvas_width/2 )-200, 500), pos_anchor=posanchors.topleft)
             # for i, option in enumerate(self.button_option_surface_list):
             #     if self.selected_cell or self.selected_cell_2:
             #         scaled_remove_button = pygame.transform.scale_by(surface=option['surface2'], factor=option['scale'])
