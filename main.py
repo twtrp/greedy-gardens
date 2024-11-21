a4487a2bf5dc15729e58473a811258d240c24364
twtr.p <piraboon77@gmail.com>
2024-11-22 00:24:19 +0700

    cancelled aspect ratio locking

--- main.py
+++ main.py
@@ -97,32 +97,32 @@ class Game:
             self.state_stack[-1].render(canvas=self.canvas)
 
         # Render canvas to screen
-        # if (constants.canvas_width, constants.canvas_height) != (self.screen_width, self.screen_height):
-        #     scaled_canvas = pygame.transform.scale(surface=self.canvas, size=(self.screen_width, self.screen_height))
-        #     utils.blit(dest=self.screen, source=scaled_canvas)
-        # else:
-        #     utils.blit(dest=self.screen, source=self.canvas)
+        if (constants.canvas_width, constants.canvas_height) != (self.screen_width, self.screen_height):
+            scaled_canvas = pygame.transform.scale(surface=self.canvas, size=(self.screen_width, self.screen_height))
+            utils.blit(dest=self.screen, source=scaled_canvas)
+        else:
+            utils.blit(dest=self.screen, source=self.canvas)
+
 
         # self.screen.fill(color=colors.black)
+        # canvas_aspect_ratio = constants.canvas_width / constants.canvas_height
+        # screen_aspect_ratio = self.screen_width / self.screen_height
 
-        canvas_aspect_ratio = constants.canvas_width / constants.canvas_height
-        screen_aspect_ratio = self.screen_width / self.screen_height
+        # if screen_aspect_ratio > canvas_aspect_ratio:
+        #     scale_factor = self.screen_height / constants.canvas_height
+        #     scaled_width = int(constants.canvas_width * scale_factor)
+        #     scaled_height = self.screen_height
+        #     offset_x = (self.screen_width - scaled_width) // 2
+        #     offset_y = 0
+        # else:
+        #     scale_factor = self.screen_width / constants.canvas_width
+        #     scaled_width = self.screen_width
+        #     scaled_height = int(constants.canvas_height * scale_factor)
+        #     offset_x = 0
+        #     offset_y = (self.screen_height - scaled_height) // 2
 
-        if screen_aspect_ratio > canvas_aspect_ratio:
-            scale_factor = self.screen_height / constants.canvas_height
-            scaled_width = int(constants.canvas_width * scale_factor)
-            scaled_height = self.screen_height
-            offset_x = (self.screen_width - scaled_width) // 2
-            offset_y = 0
-        else:
-            scale_factor = self.screen_width / constants.canvas_width
-            scaled_width = self.screen_width
-            scaled_height = int(constants.canvas_height * scale_factor)
-            offset_x = 0
-            offset_y = (self.screen_height - scaled_height) // 2
-
-        scaled_canvas = pygame.transform.scale(self.canvas, (scaled_width, scaled_height))
-        self.screen.blit(scaled_canvas, (offset_x, offset_y))
+        # scaled_canvas = pygame.transform.scale(self.canvas, (scaled_width, scaled_height))
+        # self.screen.blit(scaled_canvas, (offset_x, offset_y))
         
         # Update display
         pygame.display.update()
