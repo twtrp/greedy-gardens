8c7fcf181fadb0e80ba923bcb9606803c4e30914
twtrp <134314926+twtrp@users.noreply.github.com>
2024-11-21 18:10:07 +0700

    1.0.0

--- src/library/resources/dir.py
+++ src/library/resources/dir.py
@@ -1,5 +1,23 @@
 from src.library.core import *
+import os
+import sys
 
+
+'''Mac OS'''
+# def resource_path(relative_path):
+#     """Get the absolute path to a resource, works for PyInstaller."""
+#     try:
+#         base_path = sys._MEIPASS
+#     except AttributeError:
+#         base_path = os.path.abspath(".")
+    
+#     return os.path.join(base_path, relative_path)
+# assets = resource_path('assets')
+# data = resource_path('data')
+# src = resource_path('src')
+
+
+'''Windows and Linux'''
 assets = os.path.join('assets')
 data = os.path.join('data')
 src = os.path.join('src')
