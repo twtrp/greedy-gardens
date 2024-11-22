028d4113287266637967d24a637064a49e60d834
twtr.p <piraboon77@gmail.com>
2024-11-22 23:38:37 +0700

    1.0.4 mac

--- src/library/resources/dir.py
+++ src/library/resources/dir.py
@@ -4,23 +4,23 @@ import sys
 
 
 '''Mac OS'''
-# def resource_path(relative_path):
-#     """Get the absolute path to a resource, works for PyInstaller."""
-#     try:
-#         base_path = sys._MEIPASS
-#     except AttributeError:
-#         base_path = os.path.abspath(".")
+def resource_path(relative_path):
+    """Get the absolute path to a resource, works for PyInstaller."""
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
     
-#     return os.path.join(base_path, relative_path)
-# assets = resource_path('assets')
-# data = resource_path('data')
-# src = resource_path('src')
+    return os.path.join(base_path, relative_path)
+assets = resource_path('assets')
+data = resource_path('data')
+src = resource_path('src')
 
 
 '''Windows and Linux'''
-assets = os.path.join('assets')
-data = os.path.join('data')
-src = os.path.join('src')
+# assets = os.path.join('assets')
+# data = os.path.join('data')
+# src = os.path.join('src')
 
 fonts = os.path.join(assets, 'fonts')
 graphics = os.path.join(assets, 'graphics')
