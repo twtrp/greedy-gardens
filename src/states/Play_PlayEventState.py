371adb7a5f6e2d61d791ff4a53376c4f33e21afb
twtr.p <piraboon77@gmail.com>
2025-08-30 10:26:54 +0700

    magic fruit bug (hopefully last one ever)

--- src/states/Play_PlayEventState.py
+++ src/states/Play_PlayEventState.py
@@ -983,6 +983,30 @@ class Play_PlayEventState(BaseState):
     # Class methods
     def check_magic_fruit_collection(self, button):
         if self.parent.game_board.magic_fruit_index:
+            self.parent.magic_eventing, magic_number, cell_pos = self.parent.game_board.magic_fruit_found()
+            # print(self.parent.magic_eventing)
+            if self.parent.magic_eventing:
+                utils.sound_play(sound=sfx.magic_fruit, volume=self.game.sfx_volume)
+                if magic_number == 1:
+                    self.parent.current_event = self.parent.magic_fruit1_event
+                    # print('PlacePath Magic 1:', self.parent.magic_fruit1_event)
+                elif magic_number == 2:
+                    self.parent.current_event = self.parent.magic_fruit2_event
+                    # print('PlacePath Magic 2:', self.parent.magic_fruit2_event)
+                elif magic_number == 3:
+                    self.parent.current_event = self.parent.magic_fruit3_event
+                    # print('PlacePath Magic 3:', self.parent.magic_fruit3_event)
+                self.parent.game_board.board[cell_pos].magic_fruit = 0
+                self.parent.magicing_number = magic_number
+                current_score = getattr(self.parent, f'day{self.parent.current_day}_score')
+                new_score = current_score + 1
+                setattr(self.parent, f'day{self.parent.current_day}_score', new_score)
+                setattr(self.parent, f'magic_fruit{magic_number}_event', None)
+                
+                # Check if the path that triggered magic fruit was also a strike
+                if "strike" in self.parent.current_path:
+                    self.parent.is_striking = True
+                    
             self.parent.game_board.eval_new_tile(button.id)
             self.parent.magic_eventing, magic_number, cell_pos = self.parent.game_board.magic_fruit_found()
             if self.parent.magic_eventing:
@@ -1000,4 +1024,4 @@ class Play_PlayEventState(BaseState):
                 setattr(self.parent, f'day{self.parent.current_day}_score', new_score)
                 setattr(self.parent, f'magic_fruit{magic_number}_event', None)
                 self.parent.play_event_state= False
-                self.exit_state()
+
