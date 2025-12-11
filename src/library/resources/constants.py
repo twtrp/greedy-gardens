ff353d9c876155f9b03a91db2624b1ce0dd51480
twtr.p <piraboon77@gmail.com>
2025-12-11 18:02:16 +0700

    1.0.0

--- src/library/resources/constants.py
+++ src/library/resources/constants.py
@@ -1,5 +1,5 @@
 canvas_width = 1280    #default: 1280
 canvas_height = 720    #default: 720
-window_width = 3840    #default: 1280
-window_height = 2160   #default: 720
+window_width = 1280    #default: 1280
+window_height = 720    #default: 720
 game_version = '1.0.0'
